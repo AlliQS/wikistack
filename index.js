d244a81291a980643df4b3b5025ccb9a566f1389
Lani Tran <lanitran7@gmail.com>
2020-03-12 16:11:00 -0400

    update wikistack

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const morgan = require("morgan");
 const express = require("express");
+const { db } = require("./models");
+const layout = require('./views/layout');
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use("/", express.static("wikistack" + "public"));
+app.use('./views',express.static("wikistack" + 'public'))
 
 app.get("/", function(req, res) {
-  res.end(console.log("hello world"));
+  res.send(layout(''));
 });
+
+const PORT = 1234;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+})
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+})
