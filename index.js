51f3740856a0723e4a2a22707f2b7318c88189b2
Allison Sasso <aqsasso@gmail.com>
2020-03-12 17:14:47 -0400

    reinstalled sequalize and now it might work?

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const morgan = require("morgan");
 const express = require("express");
 const { db } = require("./models");
-const layout = require('./views/layout');
+const layout = require("./views/layout");
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use('./views',express.static("wikistack" + 'public'))
+app.use("./views", express.static("wikistack" + "public"));
 
 app.get("/", function(req, res) {
-  res.send(layout(''));
+  res.send(layout(""));
 });
 
 const PORT = 1234;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
-})
+});
 
-db.authenticate().then(() => {
-  console.log('connected to the database');
-})
+const connect = async () => {
+  // connect to database
+  try {
+    await db.sync();
+    console.log("DB is sync’d");
+  } catch (err) {
+    console.log(err);
+  }
+  //   db.authenticate().then(() => {
+  //     console.log("connected to the database");
+  //   });
+};
+
+connect();
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:1234/wikistack');
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack");
 
 module.exports = {
   db
-}
+};
